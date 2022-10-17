0a0b3468fa877fe5b2f50649f7f6a08d6e1fe81c
calvinsendawula <calvin.sendawula@strathmore.edu>
2022-10-17 10:16:49 +0300

    Create crimson_tide_restaurant.sql

---
+++ crimson_tide_restaurant.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 16, 2022 at 06:07 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crimson_tide_restaurant`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client_order`
+--
+-- Error reading structure for table crimson_tide_restaurant.client_order: #1932 - Table &#039;crimson_tide_restaurant.client_order&#039; doesn&#039;t exist in engine
+-- Error reading data for table crimson_tide_restaurant.client_order: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crimson_tide_restaurant`.`client_order`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client_order_bill`
+--
+-- Error reading structure for table crimson_tide_restaurant.client_order_bill: #1932 - Table &#039;crimson_tide_restaurant.client_order_bill&#039; doesn&#039;t exist in engine
+-- Error reading data for table crimson_tide_restaurant.client_order_bill: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crimson_tide_restaurant`.`client_order_bill`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `crimson_client`
+--
+-- Error reading structure for table crimson_tide_restaurant.crimson_client: #1932 - Table &#039;crimson_tide_restaurant.crimson_client&#039; doesn&#039;t exist in engine
+-- Error reading data for table crimson_tide_restaurant.crimson_client: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crimson_tide_restaurant`.`crimson_client`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `crimson_employee`
+--
+-- Error reading structure for table crimson_tide_restaurant.crimson_employee: #1932 - Table &#039;crimson_tide_restaurant.crimson_employee&#039; doesn&#039;t exist in engine
+-- Error reading data for table crimson_tide_restaurant.crimson_employee: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crimson_tide_restaurant`.`crimson_employee`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `crimson_menu_item`
+--
+-- Error reading structure for table crimson_tide_restaurant.crimson_menu_item: #1932 - Table &#039;crimson_tide_restaurant.crimson_menu_item&#039; doesn&#039;t exist in engine
+-- Error reading data for table crimson_tide_restaurant.crimson_menu_item: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crimson_tide_restaurant`.`crimson_menu_item`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `crimson_menu_items_ordered`
+--
+-- Error reading structure for table crimson_tide_restaurant.crimson_menu_items_ordered: #1932 - Table &#039;crimson_tide_restaurant.crimson_menu_items_ordered&#039; doesn&#039;t exist in engine
+-- Error reading data for table crimson_tide_restaurant.crimson_menu_items_ordered: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crimson_tide_restaurant`.`crimson_menu_items_ordered`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
